639390b9402f28a05fb181b07f0f3e3ecfc82709
Naphat Sanguansin <naphat.krit@gmail.com>
2015-09-27 20:26:05 -0400

    fixed bug in byte counting in HMAC update function

--- specs/HMAC_DRBG_update.v
+++ specs/HMAC_DRBG_update.v
@@ -2,12 +2,12 @@ Require Import Coqlib.
 Require Import List. Import ListNotations.
 
 Definition HMAC_DRBG_update (HMAC: list Z -> list Z -> list Z) (provided_data K V: list Z): (list Z * list Z) :=
-  let K := HMAC K (V ++ [0;0;0;0] ++ provided_data) in
+  let K := HMAC K (V ++ [0;0] ++ provided_data) in
   let V := HMAC K V in
   match provided_data with
     | [] => (K, V)
     | _::_ =>
-      let K := HMAC K (V ++ [0;0;0;1] ++ provided_data) in
+      let K := HMAC K (V ++ [0;1] ++ provided_data) in
       let V := HMAC K V in
       (K, V)
   end.
