7f690956a9bc10509cc5102b7d8459b1edfc769e
Naphat Sanguansin <naphat.krit@gmail.com>
2015-09-29 18:18:39 -0400

    finished instantiate functional specs

--- specs/DRBG_instantiate_function.v
+++ specs/DRBG_instantiate_function.v
@@ -4,29 +4,29 @@ Require Import List. Import ListNotations.
 Require Import DRBG_working_state.
 Require Import DRBG_state_handle.
 
-Inductive result :=
-| error: result
-| catastrophic_error: result
-| success: DRBG_state_handle -> result.
+Inductive instantiate_result :=
+| instantiate_error: instantiate_result
+| instantiate_catastrophic_error: instantiate_result
+| instantiate_success: DRBG_state_handle -> instantiate_result.
 
 Inductive entropy_result :=
 | entropy_error: entropy_result
 | entropy_success: list Z -> entropy_result.
 
-Definition DRBG_instantiate_function (get_entropy_input: Z -> Z -> Z -> bool -> entropy_result) (min_entropy_length max_entropy_length: Z) (get_nonce: unit -> list Z) (instantiate_algorithm: list Z -> list Z -> list Z -> Z -> DRBG_working_state) (highest_supported_security_strength: Z) (max_personalization_string_length: Z) (prediction_resistance_supported: bool) (requested_instantiation_security_strength: Z) (prediction_resistance_flag: bool) (personalization_string: list Z): result :=
-  if Z.gtb requested_instantiation_security_strength highest_supported_security_strength then error
+Definition DRBG_instantiate_function (instantiate_algorithm: list Z -> list Z -> list Z -> Z -> DRBG_working_state) (get_entropy_input: Z -> Z -> Z -> bool -> entropy_result) (min_entropy_length max_entropy_length: Z) (get_nonce: unit -> list Z) (highest_supported_security_strength: Z) (max_personalization_string_length: Z) (prediction_resistance_supported: bool) (requested_instantiation_security_strength: Z) (prediction_resistance_flag: bool) (personalization_string: list Z): instantiate_result :=
+  if Z.gtb requested_instantiation_security_strength highest_supported_security_strength then instantiate_error
   else match prediction_resistance_flag, prediction_resistance_supported with
-         | true, false => error
+         | true, false => instantiate_error
          | _,_ =>
-           if Z.gtb (Z.of_nat (length personalization_string)) max_personalization_string_length then error
+           if Z.gtb (Z.of_nat (length personalization_string)) max_personalization_string_length then instantiate_error
            else
              let security_strength := requested_instantiation_security_strength in (* TODO actually follow specs *)
              match get_entropy_input security_strength min_entropy_length max_entropy_length prediction_resistance_flag with
-               | entropy_error => catastrophic_error
+               | entropy_error => instantiate_catastrophic_error
                | entropy_success entropy_input =>
                  let nonce := get_nonce tt in
                  let initial_working_state := instantiate_algorithm entropy_input nonce personalization_string security_strength in
-                 success initial_working_state (* TODO store other info like prediction_resistance *)
+                 instantiate_success initial_working_state (* TODO store other info like prediction_resistance *)
              end
        end.
              
---
+++ specs/HMAC256_DRBG_functinoal_prog.v
@@ -0,0 +1,29 @@
+Require Import Coqlib.
+Require Import List. Import ListNotations.
+Require Import Coq.Strings.String.
+Require Import Coq.Strings.Ascii.
+Require Import functional_prog.
+Require Import HMAC256_functional_prog.
+Require Import DRBG_instantiate_function.
+Require Import HMAC_DRBG_instantiate_algorithm.
+
+Definition HMAC256_DRBG_instantiate_algorithm := HMAC_DRBG_instantiate_algorithm HMAC256.
+
+Definition HMAC256_DRBG_instantiate_function := DRBG_instantiate_function HMAC256_DRBG_instantiate_algorithm.
+
+Definition get_entropy_input_dummy (result: string) (x y z: Z) (b: bool): entropy_result := entropy_success (hexstring_to_Zlist result).
+
+Definition get_nonce_dummy (result: string) (_: unit) := hexstring_to_Zlist result.
+
+Definition test_HMAC256_DRBG_instantiate_function := HMAC256_DRBG_instantiate_function (get_entropy_input_dummy "ca851911349384bffe89de1cbdc46e6831e44d34a4fb935ee285dd14b71a7488") 0 0 (get_nonce_dummy "659ba96c601dc69fc902940805ec0ca8") 256 0 false.
+
+Definition DRBG_instantiate_check (key value: string) :=
+  let key := hexstring_to_Zlist key in
+  let value := hexstring_to_Zlist value in
+  match test_HMAC256_DRBG_instantiate_function 256 false [] with
+    | instantiate_success (value', key', _) => listZ_eq value value' = true /\ listZ_eq key key' = true
+    | _ => False
+  end.
+
+Lemma test1: DRBG_instantiate_check "302a4aba78412ab36940f4be7b940a0c728542b8b81d95b801a57b3797f9dd6e" "e75855f93b971ac468d200992e211960202d53cf08852ef86772d6490bfb53f9".
+vm_compute. auto. Qed.
