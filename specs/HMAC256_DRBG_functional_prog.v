cbfd117f700b67d89ca51038936f8edc374beccc
Naphat Sanguansin <naphat.krit@gmail.com>
2015-09-30 20:24:12 -0400

    tested generate

--- specs/HMAC256_DRBG_functional_prog.v
+++ specs/HMAC256_DRBG_functional_prog.v
@@ -4,8 +4,11 @@ Require Import Coq.Strings.String.
 Require Import Coq.Strings.Ascii.
 Require Import functional_prog.
 Require Import HMAC256_functional_prog.
+Require Import DRBG_state_handle.
 Require Import DRBG_instantiate_function.
 Require Import HMAC_DRBG_instantiate_algorithm.
+Require Import DRBG_generate_function.
+Require Import HMAC_DRBG_generate_algorithm.
 
 Definition HMAC256_DRBG_instantiate_algorithm := HMAC_DRBG_instantiate_algorithm HMAC256.
 
@@ -26,4 +29,42 @@ Definition DRBG_instantiate_check (key value: string) :=
   end.
 
 Lemma test1: DRBG_instantiate_check "302a4aba78412ab36940f4be7b940a0c728542b8b81d95b801a57b3797f9dd6e" "e75855f93b971ac468d200992e211960202d53cf08852ef86772d6490bfb53f9".
+  vm_compute. auto. Qed.
+
+Definition HMAC256_DRBG_generate_algorithm := HMAC_DRBG_generate_algorithm HMAC256 1024.
+
+Definition HMAC256_DRBG_generate_function := DRBG_generate_function HMAC256_DRBG_generate_algorithm.
+
+Definition test_HMAC256_DRBG_generate_function := HMAC256_DRBG_generate_function 128 0.
+
+Fixpoint DRBG_generate_check (state_handle: DRBG_state_handle) (internal_states: list (string * string)) (returned_bits: string) :=
+  match internal_states with
+    | [] => True
+    | (key, v)::[] =>
+      let key := hexstring_to_Zlist key in
+      let value := hexstring_to_Zlist v in
+      let returned_bits := hexstring_to_Zlist returned_bits in
+      match test_HMAC256_DRBG_generate_function state_handle 128 256 false [] with
+        | generate_success returned_bits' ((value', key', _), _, _) => listZ_eq value value' = true /\ listZ_eq key key' = true /\ listZ_eq returned_bits returned_bits' = true
+        | generate_error => False
+      end
+    | (key, v)::tl =>
+      let key := hexstring_to_Zlist key in
+      let value := hexstring_to_Zlist v in
+      match test_HMAC256_DRBG_generate_function state_handle 128 256 false [] with
+        | generate_success _ ((value', key', x), y, z) => listZ_eq value value' = true /\ listZ_eq key key' = true /\ DRBG_generate_check ((value', key', x), y, z) tl returned_bits
+        | generate_error => False
+      end
+  end.
+
+Definition test_state: DRBG_state_handle :=
+  match test_HMAC256_DRBG_instantiate_function 256 false [] with
+    | instantiate_success x => x
+    | _ => (([], [], 0), 0, false)
+  end.
+
+Lemma test_generate1: DRBG_generate_check test_state [
+                                            ("911bf7cbda4387a172a1a3daf6c9fa8e17c4bfef69cc7eff1341e7eef88d2811"%string, "bfbdcf455d5c82acafc59f339ce57126ff70b67aef910fa25db7617818faeafe"%string);
+                                            ("6dd2cd5b1edba4b620d195ce26ad6845b063211d11e591432de37a3ad793f66c"%string, "6b94e773e3469353a1ca8face76b238c5919d62a150a7dfc589ffa11c30b5b94"%string)
+                                          ] "e528e9abf2dece54d47c7e75e5fe302149f817ea9fb4bee6f4199697d04d5b89d54fbb978a15b5c443c9ec21036d2460b6f73ebad0dc2aba6e624abf07745bc107694bb7547bb0995f70de25d6b29e2d3011bb19d27676c07162c8b5ccde0668961df86803482cb37ed6d5c0bb8d50cf1f50d476aa0458bdaba806f48be9dcb8".
 vm_compute. auto. Qed.
--- specs/HMAC_DRBG_generate_algorithm.v
+++ specs/HMAC_DRBG_generate_algorithm.v
@@ -8,18 +8,24 @@ Require Import DRBG_generate_algorithm_result.
 Function HMAC_DRBG_generate_helper (HMAC: list Z -> list Z -> list Z) (key v: list Z) (requested_number_of_bytes: Z) {measure Z.to_nat requested_number_of_bytes}: (list Z * list Z) :=
   if Z.geb 0 requested_number_of_bytes then (v, [])
   else
-    let v := HMAC key v in
+    let v := HMAC v key in
     let temp := v in
     let len := 32%nat in (* TODO get this from property of HMAC *)
     let (v, rest) := HMAC_DRBG_generate_helper HMAC key v (requested_number_of_bytes - (Z.of_nat len)) in
-    (v, temp ++ v).
+    (v, temp ++ rest). 
 Proof.
   intros. rewrite Z2Nat.inj_sub by omega.
   rewrite Nat2Z.id.
-  assert ((0 <? requested_number_of_bytes) = true). admit. (* TODO *)
+  assert ((0 <? requested_number_of_bytes) = true).
+  * 
+    rewrite Z.ltb_antisym.
+    rewrite <- Z.geb_leb.
+    rewrite teq.
+    auto.
+  *
   apply Zlt_is_lt_bool in H.
   apply Z2Nat.inj_lt in H; omega.
-Qed.
+Defined.
 
 Fixpoint leftmost_items {A: Type} (items: list A) (count: Z) :=
   if Z.geb 0 count then []
@@ -29,7 +35,7 @@ Fixpoint leftmost_items {A: Type} (items: list A) (count: Z) :=
       | hd::tl => hd::leftmost_items tl (count - 1)
     end.
 
-Definition HMAC_DRBG_generate_algorithm (reseed_interval: Z) (HMAC: list Z -> list Z -> list Z) (working_state: DRBG_working_state) (requested_number_of_bytes: Z) (additional_input: list Z): DRBG_generate_algorithm_result :=
+Definition HMAC_DRBG_generate_algorithm (HMAC: list Z -> list Z -> list Z) (reseed_interval: Z) (working_state: DRBG_working_state) (requested_number_of_bytes: Z) (additional_input: list Z): DRBG_generate_algorithm_result :=
   let '(v, key, reseed_counter) := working_state in
   if Z.gtb reseed_counter reseed_interval then generate_algorithm_reseed_required
   else
