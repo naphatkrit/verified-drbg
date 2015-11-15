b693d7f3c4e7c48e69cc5ebd50d6d77fa7fd5817
Naphat Sanguansin <naphat.krit@gmail.com>
2015-11-14 19:51:41 -0500

    added timing specs for hmac compspecs

--- specs/hmac_drbg_compspecs.v
+++ specs/hmac_drbg_compspecs.v
@@ -2,6 +2,10 @@ Require Import floyd.proofauto.
 Require Import hmac_drbg.
 
 Definition CompSpecs' : compspecs.
-Proof. make_compspecs1 prog. Defined.
+Proof.
+  Time make_compspecs1 prog. (* 4843 *)
+Time Defined. (* 598 *)
 Instance CompSpecs : compspecs.
-Proof. make_compspecs2 CompSpecs'. Defined.
+Proof.
+  Time make_compspecs2 CompSpecs'. (* 525 *)
+Time Defined. (* 596 *)
