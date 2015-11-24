19a31ad6315bd481874cb5d1ad1ec84a2c0e222b
Naphat Sanguansin <naphat.krit@gmail.com>
2015-11-24 06:19:34 -0500

    switched to one step make compspecs

--- specs/hmac_drbg_compspecs.v
+++ specs/hmac_drbg_compspecs.v
@@ -1,11 +1,5 @@
 Require Import floyd.proofauto.
 Require Import hmac_drbg.
 
-Definition CompSpecs' : compspecs.
-Proof.
-  Time make_compspecs1 prog. (* 4843 *)
-Time Defined. (* 598 *)
 Instance CompSpecs : compspecs.
-Proof.
-  Time make_compspecs2 CompSpecs'. (* 525 *)
-Time Defined. (* 596 *)
+Proof. make_compspecs prog. Defined. 
