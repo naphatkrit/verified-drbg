e5ac247940b5ef4e6191015476608b225c39fa10
Naphat Sanguansin <naphat.krit@gmail.com>
2015-11-25 17:27:58 -0500

    better implementation of entropy that can fail.

--- specs/entropy.v
+++ specs/entropy.v
@@ -2,167 +2,303 @@ Require Import Coqlib.
 Require Import List. Import ListNotations.
 Require Import Coq.Logic.FunctionalExtensionality.
 
+Require Import sha.ByteBitRelations.
 Require Import sha.general_lemmas.
 
-Definition stream: Type := nat -> Z.
+Module Type ABSTRACT_ENTROPY.
 
-Fixpoint get_bytes_helper (k: nat) (s: stream) (max: nat): (list Z) :=
+Parameter stream: Type.
+
+Inductive error_code: Type :=
+| catastrophic_error
+| generic_error
+.
+
+Inductive result {X: Type}: Type :=
+| success: X -> stream -> @result X
+| error : error_code -> stream -> @result X
+.
+
+Parameter get_bytes: nat -> stream -> @result (list Z).
+Parameter get_bits: nat -> stream -> @result (list bool).
+
+Parameter get_bits_length:
+  forall k s b s',
+    success b s' = get_bits k s ->
+    length b = k.
+Parameter get_bytes_length:
+  forall k s b s',
+    success b s' = get_bytes k s ->
+    length b = k.
+Parameter get_bytes_isbyteZ:
+  forall k s b s',
+    success b s' = get_bytes k s ->
+    Forall isbyteZ b.
+End ABSTRACT_ENTROPY.
+
+Module OPTIONAL_ENTROPY <: ABSTRACT_ENTROPY.
+
+Definition stream: Type := nat -> option bool.
+
+Inductive error_code: Type :=
+| catastrophic_error
+| generic_error
+.
+
+Inductive result {X: Type}: Type :=
+| success: X -> stream -> @result X
+| error : error_code -> stream -> @result X
+.
+
+Fixpoint get_bits (k: nat) (s: stream): @result (list bool) :=
   match k with
-    | O => []
-    | S k' => (s (max - k)%nat)::(get_bytes_helper k' s max)
+    | O => success [] s
+    | S k' => match get_bits k' s with
+                | error e s' => error  e s'
+                | success b s' =>
+                  match s' O with
+                    | None => error catastrophic_error (fun i => match nat_compare i k' with
+                                                                   | Lt => s i
+                                                                   | Eq | Gt => s (1 + i)%nat
+                                                                 end
+                                                       )
+                    | Some e => success (b ++ [e]) (fun i => s (k + i)%nat)
+                  end
+              end
   end.
+Example get_bits_test1:
+  forall bits s output s',
+    bits = [Some false; Some true; Some false; Some false] ->
+    s = (fun i => nth i bits None) ->
+    success output s' = get_bits (length bits) s ->
+    s' = (fun i => s (length bits + i)%nat) /\ output = [false; true; false; false].
+Proof.
+  intros.
+  subst.
+  inv H1.
+  split.
+  extensionality i. destruct i; reflexivity.
+  reflexivity.
+Qed.
 
-Definition get_bytes (k: nat) (s: stream): (list Z * stream) :=
-  (get_bytes_helper k s k, fun i => s (k + i)%nat).
+Example get_bits_test2:
+  forall bits s output s' s'',
+    bits = [Some false; None; Some true; Some false; Some false] ->
+    s = (fun i => nth i bits None) ->
+    error catastrophic_error s' = get_bits 4%nat s ->
+    success output s'' = get_bits 4%nat s' ->
+    s'' = (fun i => s (length bits + i)%nat) /\ output = [false; true; false; false].
+Proof.
+  intros.
+  subst.
+  inv H1.
+  inv H2.
+  split.
+  extensionality i. reflexivity.
+  reflexivity.
+Qed.
 
-Lemma get_bytes_helper_n:
-  forall k s n, get_bytes_helper k s (n + k) = get_bytes_helper k (fun i => s (n + i)%nat) k.
+Example get_bits_test3:
+  forall bits s output s' s'',
+    bits = [None; Some false; Some true; Some false; Some false] ->
+    s = (fun i => nth i bits None) ->
+    error catastrophic_error s' = get_bits 4%nat s ->
+    success output s'' = get_bits 4%nat s' ->
+    s'' = (fun i => s (length bits + i)%nat) /\ output = [false; true; false; false].
+Proof.
+  intros.
+  subst.
+  inv H1.
+  inv H2.
+  split.
+  extensionality i. reflexivity.
+  reflexivity.
+Qed.
+
+Example get_bits_test4:
+  forall bits s output s' s'',
+    bits = [Some false; Some true; Some false; None; Some false] ->
+    s = (fun i => nth i bits None) ->
+    error catastrophic_error s' = get_bits 4%nat s ->
+    success output s'' = get_bits 4%nat s' ->
+    s'' = (fun i => s (length bits + i)%nat) /\ output = [false; true; false; false].
+Proof.
+  intros.
+  subst.
+  inv H1.
+  inv H2.
+  split.
+  extensionality i. reflexivity.
+  reflexivity.
+Qed.
+(*
+Fixpoint get_bits_concrete (k: nat) (s: stream) (max: nat): @result (list bool) :=
+  match k with
+    | O => success [] s
+    | S k' =>
+      match s (max - k)%nat with
+        | None => error catastrophic_error (fun i => s (1 + i)%nat)
+        | Some bit =>
+          match get_bits_concrete k' s max with
+            | error e s' => error e (fun i => match nat_compare i (max - k)%nat with
+                                                  | Gt => s' i
+                                                  | Eq | Lt => s i
+                                     end)
+            | success b s' => success (bit::b) (fun i => s (k + i)%nat)
+          end
+      end
+  end
+.
+Lemma get_bits_concrete_correct:
+  forall k s, get_bits k s = get_bits_concrete k s k.
+Proof.
+  intros k.
+  induction k as [|k']; [reflexivity|].
+  intros s.
+  simpl.
+  rewrite IHk'. clear IHk'.
+  replace (k' - k')%nat with O by omega.
+  remember (s O) as sO.
+  destruct sO.
+  {
+    (* s 0 <> None *)
+    (*
+    remember (get_bits_concrete k' s k') as result.
+    destruct result as [string s' | e s'].
+    {
+      remember (get_bits_concrete k' s (S k')) as result2.
+      destruct result2 as [string2 s'2 | e2 s'2].
+      {
+        (* case where both result return success *)
+        generalize dependent s.
+        induction k' as [|k'']; intros.
+        simpl in *. inv Heqresult. inv Heqresult2. rewrite <- HeqsO. auto.
+        simpl in *. replace (k'' - k'')%nat with O in * by omega. rewrite <- HeqsO in *.
+        rewrite IHk''.
+      }
+      unfold get_bits_concrete.
+    }
+*)
+    induction k' as [|k''].
+    {
+      simpl. rewrite <- HeqsO.
+      reflexivity.
+    }
+    simpl.
+    replace (k'' - k'')%nat with O by omega.
+    rewrite <- HeqsO in *.
+    replace (match k'' with
+             | 0%nat => S k''
+             | S l => (k'' - l)%nat
+             end) with 1%nat by (destruct k''; omega).
+    
+  }
+  {
+    destruct k' as [|k''].
+    {
+      simpl. rewrite <- HeqsO.
+      replace (fun i : nat =>
+      match nat_compare i 0 with
+      | Eq => s (S i)
+      | Lt => s i
+      | Gt => s (S i)
+      end) with (fun i => s (S i)); [reflexivity|].
+      extensionality i.
+      destruct i as [|i']; reflexivity.
+    }
+    simpl.
+    replace (k'' - k'')%nat with O by omega; rewrite <- HeqsO.
+    reflexivity.
+  }
+  simpl.
+  *)
+
+Lemma get_bits_stream_success:
+  forall k s b s',
+    success b s' = get_bits k s ->
+    s' = fun i => s (k + i)%nat.
+Proof.
+  intros k.
+  destruct k as [|k'].
+  {
+    simpl. intros. inv H. extensionality i; reflexivity.
+  }
+  simpl.
+  intros.
+  destruct (get_bits k' s); try solve [inversion H].
+  destruct (s0 0%nat); try solve [inversion H].
+  inv H.
+  reflexivity.
+Qed.
+
+Lemma get_bits_length:
+  forall k s b s',
+    success b s' = get_bits k s ->
+    length b = k.
 Proof.
   intros k.
   induction k as [|k'].
-  reflexivity.
-  intros s n.
+  {
+    intros.
+    inv H.
+    reflexivity.
+  }
+  intros until s'.
   simpl.
-  replace (n + S k' - S k')%nat with n by omega.
-  replace (n + (k' - k'))%nat with n by omega.
-  replace (n + S k')%nat with ((n + 1) + k')%nat by omega.
-  rewrite IHk' with (n:=(n+1)%nat).
-  rewrite IHk' with (n:=1%nat).
-  replace (fun i : nat => s (n + 1 + i)%nat) with (fun i : nat => s (n + (1 + i))%nat).
-  reflexivity.
-  extensionality i.
-  rewrite plus_assoc.
-  reflexivity.
+  intros H.
+  remember (get_bits k' s) as result.
+  destruct result; [|inversion H].
+  remember (s0 0%nat) as s0_0.
+  destruct s0_0; try solve [inversion H].
+  inv H.
+  rewrite app_length.
+  simpl. replace (length l + 1)%nat with (S (length l)) by omega.
+  rewrite IHk' with (s':=s0) (s:=s); auto.
 Qed.
 
-Lemma get_bytes_helper_length:
-  forall k s k', length (get_bytes_helper k s k') = k.
+Definition get_bytes (k: nat) (s: stream): @result (list Z) :=
+  match get_bits (8 * k)%nat s with
+    | success bits s' => success (bitsToBytes bits) s'
+    | error e s' => error e s'
+  end
+.
+
+Lemma get_bytes_length:
+  forall k s b s',
+    success b s' = get_bytes k s ->
+    length b = k.
 Proof.
-  intros.
-  induction k; simpl; auto.
+  unfold get_bytes; intros.
+  remember (get_bits (8 * k)%nat s) as get_bits_s. 
+  destruct get_bits_s; inv H.
+  assert (length l = (8 * k)%nat) by (apply get_bits_length with (s:=s) (s':=s0); auto).
+  apply bitsToBytes_len_gen.
+  omega.
 Qed.
 
-Lemma get_bytes_helper_Zlength:
-  forall k s k', Zlength (get_bytes_helper k s k') = Z.of_nat k.
+Lemma get_bytes_Zlength:
+  forall k s b s',
+    success b s' = get_bytes k s ->
+    Zlength b = Z.of_nat k.
 Proof.
   intros.
   rewrite Zlength_correct.
-  rewrite get_bytes_helper_length.
-  reflexivity.
+  erewrite get_bytes_length; [reflexivity | eauto].
 Qed.
 
-Lemma get_bytes_helper_isbyteZ:
-  forall k s k', (forall n, isbyteZ (s n)) -> Forall isbyteZ (get_bytes_helper k s k').
+Lemma get_bytes_isbyteZ:
+  forall k s b s',
+    success b s' = get_bytes k s ->
+    Forall isbyteZ b.
 Proof.
-  intros k s k' H.
-  induction k. constructor.
-  simpl.
-  constructor; [apply H|].
-  assumption.  
-Qed.
-
-Fixpoint get_bytes_helper_abstract k (s: stream) :=
-  match k with
-    | O => []
-    | S k' => get_bytes_helper_abstract k' s ++ [s k']
-  end.
-
-Lemma get_bytes_helper_correct:
-  forall k s, get_bytes_helper k s k = get_bytes_helper_abstract k s.
-Proof.
-  intros k.
-  induction k as [|k']; [reflexivity | ].
-  intros s.
-  simpl.
-  rewrite get_bytes_helper_n with (n:=1%nat).
-  rewrite IHk'.
-  clear.
-  replace (k' - k')%nat with O by omega.
-  revert s.
-  induction k' as [|k'']; [reflexivity | ].
   intros.
-  simpl.
-  simpl in IHk''.
-  rewrite <- IHk'' with s.
-  simpl.
-  reflexivity.
+  unfold get_bytes in H.
+  destruct (get_bits (8 * k) s); inv H.
+  eapply bitsToBytes_isbyteZ; reflexivity.
 Qed.
 
-Theorem get_bytes_additive:
-  forall l1 l2 k1 k2 s s' s'',
-    (l1, s') = get_bytes k1 s ->
-    (l2, s'') = get_bytes k2 s' ->
-    (l1 ++ l2, s'') = get_bytes (k1 + k2) s.
-Proof.
-  intros l1 l2 k1. revert l1 l2.
-  induction k1 as [| k1'].
-  {
-    (* k1 = 0 *)
-    intros.
-    unfold get_bytes in *; simpl in *.
-    assert (Hl1: l1 = []) by (change l1 with (fst (l1, s')); rewrite H; reflexivity).
-    assert (Hs': s' = s) by (change s' with (snd (l1, s')); rewrite H; simpl; extensionality i; reflexivity).
-    subst; clear H.
-    assumption.
-  }
-  (* k1 = S k1' *)
-  unfold get_bytes in *; simpl in *.
-  assert (IHK1l: forall (l1 l2 : list Z) (k2 : nat) (s s' s'' : stream),
-          (l1, s') =
-          (get_bytes_helper k1' s k1', fun i : nat => s (k1' + i)%nat) ->
-          (l2, s'') =
-          (get_bytes_helper k2 s' k2, fun i : nat => s' (k2 + i)%nat) ->
-          l1 ++ l2 =
-          get_bytes_helper (k1' + k2) s (k1' + k2)).
-  {
-    clear - IHk1'.
-    intros.
-    specialize (IHk1' l1 l2 k2 s s' s'').
-    change (l1 ++ l2) with (fst (l1 ++ l2, s'')).
-    rewrite IHk1'; try assumption. reflexivity.
-  }
-  assert (IHK1s: forall (l1 l2 : list Z) (k2 : nat) (s s' s'' : stream),
-          (l1, s') =
-          (get_bytes_helper k1' s k1', fun i : nat => s (k1' + i)%nat) ->
-          (l2, s'') =
-          (get_bytes_helper k2 s' k2, fun i : nat => s' (k2 + i)%nat) ->
-          s'' =
-          fun i : nat => s (k1' + k2 + i)%nat).
-  {
-    clear - IHk1'.
-    intros.
-    specialize (IHk1' l1 l2 k2 s s' s'').
-    change s'' with (snd (l1 ++ l2, s'')).
-    rewrite IHk1'; try assumption. reflexivity.
-  }
-  clear IHk1'.
+End OPTIONAL_ENTROPY.
 
-  intros.
-  replace (k1' + k2 - (k1' + k2))%nat with O by omega.
-  assert (Hl1: l1 = s (k1' - k1')%nat :: get_bytes_helper k1' s (S k1')) by
-  (change l1 with (fst (l1, s')); rewrite H; reflexivity).
-  assert (Hs': s' = fun i : nat => s (S (k1' + i))) by
-  (change s' with (snd (l1, s')); rewrite H; reflexivity).
-  assert (Hl2: l2 = get_bytes_helper k2 s' k2) by
-  (change l2 with (fst (l2, s'')); rewrite H0; reflexivity).
-  assert (Hs'': s'' = fun i : nat => s' (k2 + i)%nat) by
-  (change s'' with (snd (l2, s'')); rewrite H0; reflexivity).
-  destruct l1 as [|hd1 tl1]; [inversion Hl1|].
-  simpl.
-  rewrite IHK1l with (k2:=k2) (s:=fun i => s (S i)) (s':=s') (s'':=s''); try assumption.
-  Focus 2.
-  inv Hl1.
-  rewrite get_bytes_helper_n with (n:=1%nat).
-  replace (fun i : nat => s (S i)) with (fun i : nat => s (1 + i)%nat); [reflexivity | extensionality i; reflexivity].
-  inv Hl1. replace (k1' - k1')%nat with O by omega.
-  rewrite get_bytes_helper_n with (n:=1%nat).
-  replace (fun i : nat => s (S i)) with (fun i : nat => s (1 + i)%nat) by (extensionality i; reflexivity).
-  replace (fun i : nat => s (S (k1' + (k2 + i)))) with (fun i : nat => s (S (k1' + k2 + i))) by (extensionality i; rewrite plus_assoc; reflexivity).
-  reflexivity.
-Qed.
+Module ENTROPY := OPTIONAL_ENTROPY.
 
-Inductive entropy_result :=
-| entropy_error: stream -> entropy_result
-| entropy_success: (list Z * stream) -> entropy_result.
-
-Definition get_entropy (security_strength min_length max_length: Z) (prediction_resistance: bool) s := entropy_success (get_bytes (Z.to_nat min_length) s).
+Definition get_entropy (security_strength min_length max_length: Z) (prediction_resistance: bool) s := ENTROPY.get_bytes (Z.to_nat min_length) s.
